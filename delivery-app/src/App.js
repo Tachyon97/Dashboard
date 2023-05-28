b02b12b99eb1bad57157a5e8d4b6d45d2c07e78d
Farhad Mohammad <25428408+Tachyon97@users.noreply.github.com>
2023-05-28 12:15:55 +0200

    Few updated

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
 import AdminPage from './pages/AdminPage';
-import DriverPage from './pages/DriverPage';
+import React from 'react';  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import LoginPage from './pages/LoginPage';  
+import AdminPage from './pages/AdminPage';  
+import DriverPage from './pages/DriverPage';  
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/driver" element={<DriverPage />} />
-      </Routes>
-    </Router>
-  );
-}
+function App() {  
+  return (  
+  <Router>  
+  <Routes>  
+  <Route path="/" element={<LoginPage />} />  
+  <Route path="/admin" element={<AdminPage />} />  
+  <Route path="/driver" element={<DriverPage />} />  
+  </Routes>  
+  </Router>  
+  );  
+}  
 
 export default App;
--- delivery-app/src/pages/AdminPage.js
+++ delivery-app/src/pages/AdminPage.js
@@ -1,10 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from '../firebase';
+import { collection, addDoc, query, where, getDocs } from 'firebase/firestore';
+import { Container, Form, Button, Row, Col, Navbar } from 'react-bootstrap';
 
 function AdminPage() {
+  const [delivery, setDelivery] = useState({
+    address: '',
+    driver: '',
+    pickupLocation: '',
+    dropOffLocation: '',
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(db, 'deliveries'), delivery);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
+
   return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
+    <Container>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">Admin Page</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Admin</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Row>
+        <Col>
+          <h1>Create Delivery</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formAddress">
+              <Form.Label>Delivery Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter address"
+                value={delivery.address}
+                onChange={(e) =>
+                  setDelivery({ ...delivery, address: e.target.value })
+                }
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formDriver">
+              <Form.Label>Driver</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter driver's email"
+                value={delivery.driver}
+                onChange={(e) =>
+                  setDelivery({ ...delivery, driver: e.target.value })
+                }
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPickupLocation">
+              <Form.Label>Pickup Location</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter pickup location"
+                value={delivery.pickupLocation}
+                onChange={(e) =>
+                  setDelivery({ ...delivery, pickupLocation: e.target.value })
+                }
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formDropOffLocation">
+              <Form.Label>Drop-off Location</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter drop-off location"
+                value={delivery.dropOffLocation}
+                onChange={(e) =>
+                  setDelivery({ ...delivery, dropOffLocation: e.target.value })
+                }
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Create Delivery
+            </Button>
+          </Form>
+        </Col>
+
+        <Col>
+          {/* You can add components for Created Deliveries and Completed Deliveries here */}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
