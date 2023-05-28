c9f386d1bd068923fe05c8aefde793bbc15b3a38
Farhad Mohammad <25428408+Tachyon97@users.noreply.github.com>
2023-05-28 12:33:54 +0200

    .

--- delivery-app/src/index.js
+++ delivery-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'popper.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
