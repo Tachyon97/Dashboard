e92abf5dce3c395ddc67e8ed40379428ac35c2de
Farhad Mohammad <25428408+Tachyon97@users.noreply.github.com>
2023-05-28 12:16:01 +0200

    Update App.js

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -1,4 +1,3 @@
-import AdminPage from './pages/AdminPage';
 import React from 'react';  
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 import LoginPage from './pages/LoginPage';  
